d53538c21a8bc23f4122ac4d72a8171b07eca6ae
Balli Asghar <ballihacker@outlook.com>
2023-08-16 19:20:41 +0100

    Refactored: build.sh

--- build.sh
+++ build.sh
@@ -10,13 +10,13 @@ fi
 grpc_tools_node_protoc \
 --js_out=import_style=commonjs,binary:${OUT_DIR} \
 --grpc_out=grpc_js:${OUT_DIR} \
---plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
+--plugin=protoc-gen-grpc=$(npm root)/.bin/grpc_tools_node_protoc_plugin \
 -I ./proto \
 ./proto/*.proto
 
 # generate d.ts codes
 protoc \
---plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
+--plugin=protoc-gen-ts=$(npm root)/.bin/protoc-gen-ts \
 --ts_out=grpc_js:${OUT_DIR} \
 -I ./proto \
 ./proto/*.proto
